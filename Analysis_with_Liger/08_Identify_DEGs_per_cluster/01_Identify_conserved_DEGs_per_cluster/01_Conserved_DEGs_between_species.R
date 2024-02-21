bc7281be6e7b5cd6612ea643ae1c67ee2ad76b00
ytamal2 <ytamal2@mpipz.mpg.de>
2024-02-21 19:34:36 +0100

    Files - DEGs

---
+++ Analysis_with_Liger/08_Identify_DEGs_per_cluster/01_Identify_conserved_DEGs_per_cluster/01_Conserved_DEGs_between_species.R
@@ -0,0 +1,23 @@
+# Specify the projects dir containing all the functions of scRNA-seq analysis pipeline
+projects_dir <- "/home/ytamal2/Documents/2023/PhD_projects_Yasir/scExplorer/Functions/"
+
+# Functions - Markers identification
+list_1 <- list.files(paste0(projects_dir, "Library_handler"), pattern = "*.R$", full.names = TRUE) 
+sapply(list_1, source, .GlobalEnv)
+
+# Functions - Data manipulation
+list_2 <- list.files(paste0(projects_dir, "Functions_matrix_manipulation"), pattern = "*.R$", full.names = TRUE) 
+sapply(list_2, source, .GlobalEnv)
+
+# Functions - Library and packages handler
+list_3 <- list.files(paste0(projects_dir, "Functions_marker_identification"), pattern = "*.R$", full.names = TRUE) 
+sapply(list_3, source, .GlobalEnv)
+
+# Storing directory
+res_dir = "/netscratch/dep_tsiantis/grp_laurent/tamal/2024/scRNA_projects/comparative_study_of_APEX_between_two_species/Analysis_with_Liger/Analysis_outputs"
+
+load("/netscratch/dep_tsiantis/grp_laurent/tamal/2024/scRNA_projects/comparative_study_of_APEX_between_two_species/Analysis_with_Liger/Seurat_objects/seurat_object_of_K_50.RData")
+
+Idents(integrated.data) <- "RNA_snn_res.0.2"
+
+cluster_conserved_marker_finder(seuratObject = integrated.data, grouping_variable = "Species", store_dir = res_dir, DEGtest = "wilcox")
